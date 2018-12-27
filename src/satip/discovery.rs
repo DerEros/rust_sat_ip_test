359ccd75ec273f6f2253abe2fe1a7d7994d0cfec
DerEros <dereros@noreply.users.github.com>
2018-12-27 18:07:37 +0100

    Some more refactoring

--- src/satip/discovery.rs
+++ src/satip/discovery.rs
@@ -23,8 +23,7 @@ USER-AGENT: Linux/1.0 UPnP/1.1 ernasatip/1.0
 
 pub fn discover_servers(bind_address: &str) -> io::Result<String> {
     info!("Querying for SAT>IP servers using '{}'", bind_address);
-    let socket = UdpSocket::bind(bind_address)?;
-    debug!("Binding to '{}'", socket.local_addr()?);
+    let socket = open_socket(bind_address)?;
 
     send_discovery_request(&socket)?;
     let (notify_message, source) = receive_notify_message(&socket)?;
@@ -33,11 +32,17 @@ pub fn discover_servers(bind_address: &str) -> io::Result<String> {
     Ok(notify_message)
 }
 
-pub fn send_discovery_request(socket: &UdpSocket) -> io::Result<usize> {
+fn open_socket(bind_address: &str) -> io::Result<UdpSocket> {
+    let socket = UdpSocket::bind(bind_address)?;
+    debug!("Binding to '{}'", socket.local_addr()?);
+    Ok(socket)
+}
+
+fn send_discovery_request(socket: &UdpSocket) -> io::Result<usize> {
     socket.send_to(search_servers_request().as_bytes(), discovery_address())
 }
 
-pub fn receive_notify_message(socket: &UdpSocket) -> io::Result<(String, SocketAddr)> {
+fn receive_notify_message(socket: &UdpSocket) -> io::Result<(String, SocketAddr)> {
     let mut buf = [0; 5000];
     let (_, source) = socket.recv_from(&mut buf)?;
 
