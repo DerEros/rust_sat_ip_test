428019ab2713a1a49b80e70116e7e9cf15e9f215
DerEros <dereros@noreply.users.github.com>
2019-01-04 23:17:29 +0100

    Revert back to Result instead of Futures

--- src/satip/discovery.rs
+++ src/satip/discovery.rs
@@ -7,36 +7,35 @@ use tokio::prelude::Future;
 use std::str::FromStr;
 use tokio::prelude::future::IntoFuture;
 
-//fn search_servers_request() -> String {
+//fn search_servers_request(target_address: &SocketAddr, user_agent: &str) -> String {
 //    format!("M-SEARCH * HTTP/1.1
 //HOST: {}
 //MAN: \"ssdp:discover\"
 //MX: 2
 //ST: urn:ses-com:device:SatIPServer:1
-//USER-AGENT: Linux/1.0 UPnP/1.1 ernasatip/1.0
+//USER-AGENT: {}
 //\r\n
-//", discovery_address())
+//", target_address, user_agent)
 //}
 
 #[derive(Debug)]
 struct DiscoveryContext {
     pub config: Config,
-    pub socket_addr: SocketAddr,
+    pub broadcast_address: SocketAddr,
     pub socket: UdpSocket
 }
 
 impl DiscoveryContext {
-    fn new(config: Config) -> impl Future<Item = DiscoveryContext, Error = Error> {
-        parse_address_future(config.bind_address)
-            .and_then(move |parsed_address| {
-                let udp_socket_future = bind_udp_socket(parsed_address);
+    fn new(config: Config) -> Result<DiscoveryContext, Error> {
+        let broadcast_address = parse_address(config.discovery_broadcast_address)?;
+        let bind_address = parse_address(config.bind_address)?;
+        let socket = bind_udp_socket(bind_address)?;
 
-                udp_socket_future.map(move |socket| DiscoveryContext {
-                    config,
-                    socket_addr: parsed_address,
-                    socket
-                })
-            })
+        Ok(DiscoveryContext {
+            config,
+            broadcast_address,
+            socket
+        })
     }
 }
 
@@ -45,22 +44,23 @@ pub fn discover_satip_servers(config: Config) -> impl Future<Item = (), Error = 
 
     let discovery_context = DiscoveryContext::new(config);
 
-    discovery_context.map(|context| debug!("Using discovery context:\n{:?}", context))
+    discovery_context
+        .map(|context| debug!("Using discovery context:\n{:?}", context))
+        .into_future()
 }
 
-fn parse_address_future(address_string: &str) -> impl Future<Item = SocketAddr, Error = Error> {
+fn parse_address(address_string: &str) -> Result<SocketAddr, Error> {
     SocketAddr::from_str(address_string)
-        .into_future()
         .map_err(|_| Error {
             error_type: ErrorType::InvalidIpFormat,
             message: format!("Could not parse address")
         })
 }
 
-fn bind_udp_socket(socket_address: SocketAddr) -> impl Future<Item = UdpSocket, Error = Error> {
+fn bind_udp_socket(socket_address: SocketAddr) -> Result<UdpSocket, Error> {
     UdpSocket::bind(&socket_address)
         .map_err(|err| Error {
                 error_type: ErrorType::CouldNotBindUdpSocket,
                 message: format!("Unable to bind to UDP socket. Cause: {}", err)
-        }).into_future()
+        })
 }
